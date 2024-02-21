69eb0fa336a6981911bfb53087c44386b69d4986
KhaledAlkilani <khaledkelany@gmail.com>
2024-02-21 13:39:04 +0200

    Add project files.

---
+++ PullRequestDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PullRequestDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pull request assignment hello");
+        }
+    }
+};
